a299b7b971d04dfed8a9dd63e523a664da8a2da9
jonaskao123 <161675260+jonaskao123@users.noreply.github.com>
2024-03-19 20:31:15 +0800

    Add files via upload

--- hw2.R
+++ hw2.R
@@ -35,20 +35,22 @@ calculate <- function(target, badthre, input, output) {
       FP <- subset(nonfiltered_data, reference == target)
       null_P <- sum(data$reference == "bad") / nrow(data)
       
-      sensitivity <- round(sum(filtered_data$reference == target) / sum(data$reference == target), 2)
-      specificity <- 1 - round(sum(filtered_data$reference != target) / sum(data$reference != target), 2)
-      precision <- round(sum(filtered_data$reference == target) / nrow(filtered_data), 2)
-      F1 <- round(round(2 * sensitivity * precision / (sensitivity + precision), 3), 2)
-      logLikelihood <- round(sum(log(c(1 - TP$pred.score, FN$pred.score, TN$pred.score, 1 - FP$pred.score))), 2)
+      sensitivity <- sum(filtered_data$reference == target) / sum(data$reference == target)
+      specificity <- 1 - sum(filtered_data$reference != target) / sum(data$reference != target)
+      precision <- sum(filtered_data$reference == target) / nrow(filtered_data)
+      F1 <- 2 * sensitivity * precision / (sensitivity + precision)
+      logLikelihood <- sum(log(c(1 - TP$pred.score, FN$pred.score, TN$pred.score, 1 - FP$pred.score)))
       logLikelihood_null <- with(data, sum(ifelse(reference == "bad", log(null_P), log(1-null_P))))
       pseudoR2 <- round(1 -  logLikelihood / logLikelihood_null, 2)
-      return(c(sensitivity = sensitivity, specificity = specificity, F1 = F1, logLikelihood = logLikelihood, pseudoRsquared = pseudoR2))
+      return(round(c(sensitivity = sensitivity, specificity = specificity, F1 = F1, logLikelihood = logLikelihood, pseudoRsquared = pseudoR2), 2))
     }
     
   })
   
   results_df <- do.call(rbind, results)
-  method <- gsub("\\.csv", "", names(method_data)) 
+  method <- gsub("\\.csv", "", names(method_data))
+  method <- gsub("data/set1/", "", method)
+
   results_df <- cbind(method, results_df)
   results_df <- data.frame(results_df)
 
@@ -56,7 +58,7 @@ calculate <- function(target, badthre, input, output) {
   write.csv(results_df, file = output, quote = F, row.names = F)
 }
 
-# calculate("bad", 0.5, "method1.csv method2.csv", "output1.csv")
-# calculate("bad", 0.4, "method1.csv method3.csv method5.csv", "output2.csv")
-# calculate("good", 0.6, "method2.csv method4.csv method6.csv", "output3.csv")
+calculate("bad", 0.5, "HW2_DATA/method1.csv HW2_DATA/method2.csv", "HW2_DATA/output1.csv")
+calculate("bad", 0.4, "HW2_DATA/method1.csv HW2_DATA/method3.csv HW2_DATA/method5.csv", "HW2_DATA/output2.csv")
+calculate("good", 0.6, "HW2_DATA/method2.csv HW2_DATA/method4.csv HW2_DATA/method6.csv", "HW2_DATA/output3.csv")
                     
