bb8fdb1f86e93626a5c9d3e38c59b4b7e181f4ca
jonaskao123 <161675260+jonaskao123@users.noreply.github.com>
2024-04-08 13:03:31 +0800

    Add files via upload

--- hw3.R
+++ hw3.R
@@ -41,7 +41,7 @@ k_fold <- function(k, input_file, output_file){
       train_labels <- c(train_labels, missing_labels)
     }
     
-    model <- randomForest(x = train_data, y = train_labels)
+    model <- randomForest(x = train_data, y = train_labels, ntree = 500, mtry = 65, maxnodes = 20)
     
     train_pred <- predict(model, newdata = train_data)
     train_accuracy[i] <- mean(train_pred == train_labels)
@@ -70,4 +70,4 @@ k_fold <- function(k, input_file, output_file){
 }
 
 # Example usage:
-# model <- k_fold(5, 'HW3_DATA/Archaeal_tfpssm.csv', 'HW3_DATA/performance.csv')
+model <- k_fold(5, 'HW3_DATA/Archaeal_tfpssm.csv', 'HW3_DATA/performance.csv')
